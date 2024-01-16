925a56675dbb074df242baed2b9b9369f7466588
chewr <5873755+chewr@users.noreply.github.com>
2024-01-16 09:24:10 -0800

    Add work zsh template

--- init.sh
+++ init.sh
@@ -16,11 +16,20 @@ fi
 PRIVATE_DIR="${REPO_DIR}/private"
 mkdir -p ${PRIVATE_DIR}
 
+# TODO: toggle all this with a work flag
+# TODO: read user input for templating as much of this as possible
 GITCONFIG_WORK="${PRIVATE_DIR}/gitconfig-work"
 if ! test -f ${GITCONFIG_WORK}; then
     cp templates/gitconfig-work ${GITCONFIG_WORK}
     vim ${GITCONFIG_WORK}
 fi
+
+ZSHCONFIG_WORK="${PRIVATE_DIR}/zshrc-work"
+if ! test -f ${ZSHCONFIG_WORK}; then
+    cp templates/zshrc-work ${ZSHCONFIG_WORK}
+    vim ${ZSHCONFIG_WORK}
+fi
+
 
 # Run dotsync for the first time
 ${DOTFILES_TARGET}/dotsync/bin/dotsync -L
