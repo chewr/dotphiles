a183d692a65d479053789064a44511297003863b
chewr <5873755+chewr@users.noreply.github.com>
2024-09-30 17:25:04 -0700

    Init script updates + non work mode

--- init.sh
+++ init.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 
-DOTFILES_TARGET="~/.dotfiles"
+DOTFILES_TARGET="${HOME}/.dotfiles"
+
+echo "Dotfiles target: ${DOTFILES_TARGET}"
 
 # Set up symlink
 REPO_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-if test ${DOTFILES_TARGET}; then
+if test -d ${DOTFILES_TARGET} || test -L ${DOTFILES_TARGET}; then
     echo "${DOTFILES_TARGET} exists."
 else
-    echo "Linking ~/.dotfiles to ${REPO_DIR}"
-    ln -s ${REPO_DIR} ~/.dotfiles
+    echo "Linking ${DOTFILES_TARGET} to ${REPO_DIR}"
+    ln -s ${REPO_DIR} ${DOTFILES_TARGET}
+    sleep 0.5
 fi
 
 # Set up private directory
 PRIVATE_DIR="${REPO_DIR}/private"
 mkdir -p ${PRIVATE_DIR}
 
-# TODO: toggle all this with a work flag
 # TODO: read user input for templating as much of this as possible
-GITCONFIG_WORK="${PRIVATE_DIR}/gitconfig-work"
-if ! test -f ${GITCONFIG_WORK}; then
-    cp templates/gitconfig-work ${GITCONFIG_WORK}
-    vim ${GITCONFIG_WORK}
-fi
+setup_config() {
+    CONF_FILE="${1}"
+    TARGET_PATH="${PRIVATE_DIR}/${CONF_FILE}"
+    if ! test -f ${TARGET_PATH}; then
+	    cp templates/${CONF_FILE} ${TARGET_PATH}
+	    vim ${TARGET_PATH}
+    fi
+}
 
-ZSHCONFIG_WORK="${PRIVATE_DIR}/zshrc-work"
-if ! test -f ${ZSHCONFIG_WORK}; then
-    cp templates/zshrc-work ${ZSHCONFIG_WORK}
-    vim ${ZSHCONFIG_WORK}
-fi
+# Set workmode or homemode
+setup_config "dotfiles-config"
 
+source ${PRIVATE_DIR}/dotfiles-config
+if [[ "${IS_WORKMODE}" = "true" ]]; then
+    setup_config "gitconfig-work"
+    setup_config "zshrc-work"
+fi
 
 # Run dotsync for the first time
 ${DOTFILES_TARGET}/dotsync/bin/dotsync -L
